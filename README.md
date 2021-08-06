aac510378c0a476bf1c6b73025ea30bd694cd616
CarsonAurum <carsonaurum1@icloud.com>
2021-08-06 10:18:07 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Animatables
+
+A description of this package.
---
+++ Tests/AnimatablesTests/AnimatablesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Animatables
+
+final class AnimatablesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Animatables().text, "Hello, World!")
+    }
+}
