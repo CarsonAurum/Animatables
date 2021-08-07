3d8c36b86a7b55f3e3589108256ad81c5df5ac72
CarsonAurum <carsonaurum1@icloud.com>
2021-08-07 14:01:19 -0500

    AtomSpinner

--- Sources/Spinners/AtomSpinner.swift
+++ Sources/Spinners/AtomSpinner.swift
@@ -5,4 +5,53 @@
 //  Created by Carson Rau on 8/7/21.
 //
 
-import Foundation
+import SwiftUI
+
+public struct AtomSpinner: View {
+    public enum AnimationType {
+        case spring
+        case ease
+        internal var animation: Animation {
+            switch self {
+            case .spring:
+                return Animation.interpolatingSpring(stiffness: 60, damping: 13).repeatForever(autoreverses: true)
+            case .ease:
+                return Animation.easeInOut(duration: 2).delay(0.5).repeatForever(autoreverses: true)
+            }
+        }
+    }
+
+    @Binding var scaleRotate: Bool
+    @State var animationType: AnimationType
+    public var body: some View {
+        ZStack {
+            Image("Atom", bundle: .module)
+                .scaleEffect(scaleRotate ? 1 : 0.4)
+                .rotationEffect(.degrees(scaleRotate ? 360 : 0), anchor: .center)
+                .animation(animationType.animation)
+                .onAppear {
+                    scaleRotate.toggle()
+                }
+        }
+    }
+}
+
+#if DEBUG
+struct AtomSpinnerContainer: View {
+    @State private var animating: Bool = false
+
+    var body: some View {
+        ZStack {
+            Color(red: 0.0, green: 0.0, blue: 0.0)
+                .scaleEffect(1.2)
+            AtomSpinner(scaleRotate: $animating, animationType: .spring)
+        }
+    }
+}
+
+struct AtomSpinner_Previews: PreviewProvider {
+    static var previews: some View {
+        AtomSpinnerContainer()
+    }
+}
+#endif
