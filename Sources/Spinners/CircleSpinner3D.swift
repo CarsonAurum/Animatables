0dc7d74bfada81a6b41a3521c511203da074a607
CarsonAurum <carsonaurum1@icloud.com>
2021-08-09 21:39:27 -0500

    Scaleable Spinner!

--- Sources/Spinners/CircleSpinner3D.swift
+++ Sources/Spinners/CircleSpinner3D.swift
@@ -8,58 +8,49 @@
 import SwiftUI
 
 public struct CircleSpinner3D: View {
-    @Binding public var spinOuter: Bool
-    @Binding public var spinMiddle: Bool
-    @Binding public var spinInner: Bool
+    @Binding public var animating: Bool
     public var color: Color = .blue
     public var body: some View {
-        ZStack {
-            Circle()
-                .stroke(lineWidth: 5.0)
-                .foregroundColor(color)
-                .frame(width: 100, height: 100)
-                .rotation3DEffect(
-                    .degrees(spinOuter ? 180 : 1),
-                    axis: (x: spinOuter ? 1 : 0, y: 0, z: 0)
-                )
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                .onAppear {
-                    spinOuter.toggle()
-                }
-            Circle()
-                .stroke(lineWidth: 5.0)
-                .foregroundColor(color)
-                .frame(width: 60, height: 60)
-                .rotation3DEffect(
-                    .degrees(spinMiddle ? 360 : 1),
-                    axis: (x: 0, y: spinMiddle ? 1 : 0, z: 0)
-                )
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                .onAppear {
-                    spinMiddle.toggle()
-                }
-            Circle()
-                .stroke(lineWidth: 5.0)
-                .foregroundColor(color)
-                .frame(width: 20, height: 20)
-                .rotation3DEffect(
-                    .degrees(spinInner ? 360 : 0),
-                    axis: (x: spinInner ? 1 : 0, y: spinInner ? 1 : 0 , z: 0)
-                )
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                .onAppear {
-                    spinInner.toggle()
-                }
+        GeometryReader { proxy in
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: proxy.size.width / 20)
+                    .foregroundColor(color)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+                    .rotation3DEffect(
+                        .degrees(animating ? 180 : 1),
+                        axis: (x: animating ? 1 : 0, y: 0, z: 0)
+                    )
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
+                Circle()
+                    .stroke(lineWidth: proxy.size.width / 20)
+                    .foregroundColor(color)
+                    .frame(width: proxy.size.width * 0.6, height: proxy.size.height * 0.6)
+                    .rotation3DEffect(
+                        .degrees(animating ? 360 : 1),
+                        axis: (x: 0, y: animating ? 1 : 0, z: 0)
+                    )
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
+                Circle()
+                    .stroke(lineWidth: proxy.size.width / 20)
+                    .foregroundColor(color)
+                    .frame(width: proxy.size.width * 0.2, height: proxy.size.height * 0.2)
+                    .rotation3DEffect(
+                        .degrees(animating ? 540 : 0),
+                        axis: (x: animating ? 1 : 0, y: animating ? 1 : 0 , z: 0)
+                    )
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true))
+            }
+            .onAppear {
+                animating.toggle()
+            }
         }
     }
 }
 
 #if DEBUG
 struct SpinnerContainer: View {
-    @State private var spinX: Bool = false
-    @State private var spinY: Bool = false
-    @State private var spinXY: Bool = false
-
+    @State private var animate: Bool = false
     var body: some View {
         ZStack {
             RadialGradient(
@@ -69,11 +60,8 @@ struct SpinnerContainer: View {
                 endRadius: 500
             )
             .edgesIgnoringSafeArea(.all)
-            CircleSpinner3D(
-                spinOuter: $spinX,
-                spinMiddle: $spinY,
-                spinInner: $spinXY
-            )
+            CircleSpinner3D(animating: $animate)
+                .frame(width: 100, height: 100)
         }
     }
 }
